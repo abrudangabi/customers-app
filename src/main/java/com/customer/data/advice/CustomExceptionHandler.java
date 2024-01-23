800f15484c74e7c7508bba4fcc030b2efc715789
abrudangabi <abrudan_gabi@yahoo.com>
2024-01-23 18:04:42 +0200

    Exception handling added
    Exception handling added to service operations

---
+++ src/main/java/com/customer/data/advice/CustomExceptionHandler.java
@@ -0,0 +1,35 @@
+package com.customer.data.advice;
+
+import com.customer.data.exception.CustomerValidationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class CustomExceptionHandler {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleInvalidArgument(MethodArgumentNotValidException exception) {
+        Map<String, String> map =  new HashMap<>();
+        exception.getBindingResult().getFieldErrors().forEach(fieldError -> {
+            map.put(fieldError.getField(), fieldError.getDefaultMessage());
+        });
+
+        return map;
+
+    }
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(CustomerValidationException.class)
+    public Map<String, String> handleCustomerValidationException(CustomerValidationException exception) {
+        Map<String, String> map =  new HashMap<>();
+        map.put("errorMessage", exception.getMessage());
+        return map;
+    }
+}
