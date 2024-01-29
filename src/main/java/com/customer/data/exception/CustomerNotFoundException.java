fed6e1a2c73348c0ec4ae3b7d0f2341a6b077063
abrudangabi <abrudan_gabi@yahoo.com>
2024-01-29 16:23:38 +0200

    Refactoring code
    Removing unused code and adding build tests for maven

--- src/main/java/com/customer/data/exception/CustomerNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.customer.data.exception;
-
-public class CustomerNotFoundException extends RuntimeException{
-
-    CustomerNotFoundException(Long id) {
-        super("Could not find customer " + id);
-    }
-}
